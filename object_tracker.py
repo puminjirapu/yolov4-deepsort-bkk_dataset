1a3b5db1b86a0c94c87424557d030f54f95b1a6f
Dage Tiwattanasuk <109887239+Dage2544@users.noreply.github.com>
2023-03-17 00:45:51 +0700

    can save to .csv

--- object_tracker.py
+++ object_tracker.py
@@ -25,7 +25,6 @@ from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
 
 import pickle
-import csv
 
 class TrackObj():
   def __init__(self, id, cls, xmin, ymin, xmax, ymax):
@@ -213,11 +212,8 @@ def main(_argv):
 
         # Call the tracker
         tracker.predict()
-        print("tracker count:",len(tracker.tracks)) #count check
-        
         tracker.update(detections)
-        print("tracker count:",len(tracker.tracks)) #count check
-        
+
         # update tracks
         objs = []
         for track in tracker.tracks:
@@ -231,4 +227,40 @@ def main(_argv):
             color = [i * 255 for i in color]
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-            cv2.putText(frame, class_name + "-" + str(track.track_id)
+            cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+
+        # if enable info flag then print details about each track
+            if FLAGS.info:
+                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+        
+        # create TrackObj
+            obj = TrackObj(track.track_id, class_name, int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]) )
+            objs.append(obj)
+        
+        frames.append(objs)
+
+        # calculate frames per second of running detections
+        fps = 1.0 / (time.time() - start_time)
+        print("FPS: %.2f" % fps)
+        result = np.asarray(frame)
+        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        
+        if not FLAGS.dont_show:
+            cv2.imshow("Output Video", result)
+        
+        # if output flag is set, save video file
+        if FLAGS.output:
+            out.write(result)
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+    # save to pickle
+    save_path = os.path.join('/'.join(FLAGS.output.split('/')[:-1]), 'state.bin')
+    with open(save_path, "wb") as f: # "wb" because we want to write in binary mode
+        pickle.dump(frames, f)
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
