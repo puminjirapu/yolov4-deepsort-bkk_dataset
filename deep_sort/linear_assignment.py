30f9a8c00fa05ca6716656712d553c5a3fab90c3
Dage Tiwattanasuk <109887239+Dage2544@users.noreply.github.com>
2023-03-17 00:30:13 +0700

    no jumping
    2*gating_distance > gating_threshold

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -187,5 +187,5 @@ def gate_cost_matrix(
         track = tracks[track_idx]
         gating_distance = kf.gating_distance(
             track.mean, track.covariance, measurements, only_position)
-        cost_matrix[row, gating_distance > gating_threshold] = gated_cost
+        cost_matrix[row, 2*gating_distance > gating_threshold] = gated_cost
     return cost_matrix
