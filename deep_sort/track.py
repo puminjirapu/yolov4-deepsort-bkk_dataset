19aef1bd91b63e1273196a05cd371244fb01558e
Dage Tiwattanasuk <109887239+Dage2544@users.noreply.github.com>
2023-04-14 01:27:01 +0700

    Update track.py

--- deep_sort/track.py
+++ deep_sort/track.py
@@ -64,7 +64,7 @@ class Track:
     """
 
     def __init__(self, mean, covariance, track_id, n_init, max_age,
-                 feature=None, class_name=None):
+                 feature=None, class_name=None, class_confidence=None):
         self.mean = mean
         self.covariance = covariance
         self.track_id = track_id
@@ -80,6 +80,7 @@ class Track:
         self._n_init = n_init
         self._max_age = max_age
         self.class_name = class_name
+        self.class_confidence = class_confidence
 
     def to_tlwh(self):
         """Get current position in bounding box format `(top left x, top left y,
@@ -112,6 +113,9 @@ class Track:
     
     def get_class(self):
         return self.class_name
+
+    def get_confidence(self):
+        return self.class_confidence
 
     def predict(self, kf):
         """Propagate the state distribution to the current time step using a
@@ -147,6 +151,8 @@ class Track:
         self.time_since_update = 0
         if self.state == TrackState.Tentative and self.hits >= self._n_init:
             self.state = TrackState.Confirmed
+        
+        self.class_confidence = detection.get_confidence()
 
     def mark_missed(self):
         """Mark this track as missed (no association at the current time step).
